60cfdb1c2431688d889c417ce9eb1ac3d6ab536e
Grayvox <144566632+Grayvox@users.noreply.github.com>
2023-12-03 22:45:53 -0500

    Clarify results and align text
    * Edits the possible results to be more specific
    
    * Aligns the result box's text to the center

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         resultBoard.textContent = `It's a tie! Thou hath both chosen thy same`;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
-        resultBoard.textContent = `Paper covers thou's rock. Computer tis victorious!`;
+        resultBoard.textContent = `Computer's paper covers thou's rock. Computer tis victorious!`;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
-        resultBoard.textContent = `Rock smashes thou's scissors. Player tis victorious!`;
+        resultBoard.textContent = `Thou's Rock smashes Computer's scissors. Player tis victorious!`;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
-        resultBoard.textContent = `Paper covers thou's rock. Player tis victorious!`;
+        resultBoard.textContent = `Thou's paper covers Computer's rock. Player tis victorious!`;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
-        resultBoard.textContent = `Scissors cuts thou's paper. Computer tis victorious!`;
+        resultBoard.textContent = `Computer's scissors cuts thou's paper. Computer tis victorious!`;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
-        resultBoard.textContent = `Scissors cuts thou's paper. Player tis victorious!`;
+        resultBoard.textContent = `Thou's scissors cuts computer's paper. Player tis victorious!`;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
-        resultBoard.textContent = `Paper covers thou's rock. Computer tis victorious!`;
+        resultBoard.textContent = `Computer's paper covers thou's rock. Computer tis victorious!`;
     } else {
         resultBoard.textContent = `Error! One of thy selves chose a false weapon!`;
     }
